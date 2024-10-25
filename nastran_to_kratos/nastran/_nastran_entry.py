27871eef345afa6798475b1d6a13a4761c2c977f
unexcellent <>
2024-10-25 13:06:16 +0200

    refactor grid field reading methods to avoid code duplictation

--- nastran_to_kratos/nastran/_nastran_entry.py
+++ nastran_to_kratos/nastran/_nastran_entry.py
@@ -1,12 +1,52 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from typing import TypeVar
+
+T = TypeVar("T")
 
 
 class _NastranEntry(ABC):
     """The template for any single nastran element.
 
-    This class should not be used directly."""
+    This class should not be used directly.
+    """
 
     @classmethod
     @abstractmethod
-    def read(cls, raw_entry: list[str]) -> "_NastranEntry":
+    def read(cls, raw_entry: list[str]) -> _NastranEntry:
         """Create this class from the relevant entry in a nastran file."""
+
+    @classmethod
+    def _read_optional_field(
+        cls,
+        raw_entry: list[str],
+        field_index: int,
+        target_class: type[T],
+        default_value: T,
+    ) -> T:
+        """Read an optional field from a nastran entry.
+
+        Most fields in nastran are optional. In that case they are either not present at all or just
+        entirely blank spaces. If a field exists and has content, this function strips the raw field
+        string of all blank spaces and converts it to the respective class (like int, float, str,
+        ...). If not, a default value is returned (like None, 0.0, ...).
+
+        Args:
+            raw_entry: entire list of fields in the entry, that is also passed to read()
+            field_index: index of the relevant field in the raw entry
+            target_class: class this entry should be parsed into if it is present
+            default_value (T): default value to be returned if the field is not present
+
+        Returns:
+            either the string parsed into the target class or the default value
+
+        """
+        try:
+            field_is_empty = raw_entry[field_index] == " " * 8
+        except IndexError:
+            field_is_empty = True
+
+        if field_is_empty:
+            return default_value
+        return target_class(raw_entry[field_index].strip())
--- nastran_to_kratos/nastran/grid.py
+++ nastran_to_kratos/nastran/grid.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 from ._nastran_entry import _NastranEntry
@@ -17,66 +19,15 @@ class Grid(_NastranEntry):
     seid: int = 0
 
     @classmethod
-    def read(cls, raw_entry: list[str]) -> "Grid":
+    def read(cls, raw_entry: list[str]) -> Grid:
+        """Construct a grid from the entry in a nastran file."""
         return Grid(
             id=int(raw_entry[1]),
-            cp=cls._read_cp(raw_entry),
-            x1=float(raw_entry[3]),
-            x2=float(raw_entry[4]),
-            x3=float(raw_entry[5]),
-            cd=cls._read_cd(raw_entry),
-            ps=cls._read_ps(raw_entry),
-            seid=cls._read_seid(raw_entry),
+            cp=cls._read_optional_field(raw_entry, 2, int, None),
+            x1=cls._read_optional_field(raw_entry, 3, float, 0.0),
+            x2=cls._read_optional_field(raw_entry, 4, float, 0.0),
+            x3=cls._read_optional_field(raw_entry, 5, float, 0.0),
+            cd=cls._read_optional_field(raw_entry, 6, int, None),
+            ps=cls._read_optional_field(raw_entry, 7, str, None),
+            seid=cls._read_optional_field(raw_entry, 8, int, 0),
         )
-
-    @classmethod
-    def _read_cp(cls, raw_entry: list[str]) -> int | None:
-        field_index = 2
-
-        field_is_empty = raw_entry[field_index] == " " * 8
-        if field_is_empty:
-            return None
-        else:
-            return int(raw_entry[field_index])
-
-    @classmethod
-    def _read_cd(cls, raw_entry: list[str]) -> int | None:
-        field_index = 6
-
-        try:
-            field_is_empty = raw_entry[field_index] == " " * 8
-        except IndexError:
-            field_is_empty = True
-
-        if field_is_empty:
-            return None
-        else:
-            return int(raw_entry[field_index])
-
-    @classmethod
-    def _read_ps(cls, raw_entry: list[str]) -> str | None:
-        field_index = 7
-
-        try:
-            field_is_empty = raw_entry[field_index] == " " * 8
-        except IndexError:
-            field_is_empty = True
-
-        if field_is_empty:
-            return None
-        else:
-            return raw_entry[field_index].strip()
-
-    @classmethod
-    def _read_seid(cls, raw_entry: list[str]) -> int:
-        field_index = 8
-
-        try:
-            field_is_empty = raw_entry[field_index] == " " * 8
-        except IndexError:
-            field_is_empty = True
-
-        if field_is_empty:
-            return 0
-        else:
-            return int(raw_entry[field_index])
