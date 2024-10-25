07b0fad8366435f87778f6455406ed38f1066ef2
unexcellent <>
2024-10-25 16:30:22 +0200

    implement Force.read()

--- nastran_to_kratos/nastran/entries/__init__.py
+++ nastran_to_kratos/nastran/entries/__init__.py
@@ -1,7 +1,8 @@
 """For each supported entry type (like CROD, GRID, ...) one class exists in this package."""
 
 from .crod import Crod
+from .force import Force
 from .grid import Grid
 from .prod import Prod
 
-__all__ = ["Crod", "Grid", "Prod"]
+__all__ = ["Crod", "Force", "Grid", "Prod"]
---
+++ test/nastran/entries/test_force.py
@@ -0,0 +1,21 @@
+from nastran_to_kratos.nastran.entries import Force
+
+import pytest
+
+
+def test_read__all_fields_have_values():
+    raw_entry = ["FORCE", "2", "5", "6", "2.9", "0.0", "1.0", "0.0"]
+
+    actual = Force.read(raw_entry)
+    assert actual == Force(sid=2, g=5, cid=6, f=2.9, n1=0.0, n2=1.0, n3=0.0)
+
+
+def test_read__optionale_fields():
+    raw_entry = ["FORCE", "2", "5", "        ", "2.9", "0.0", "1.0", "0.0"]
+
+    actual = Force.read(raw_entry)
+    assert actual == Force(sid=2, g=5, cid=0, f=2.9, n1=0.0, n2=1.0, n3=0.0)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-v"])
