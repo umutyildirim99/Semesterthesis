45815d01a593c03f60a4a352893effcb4927b3cc
unexcellent <>
2024-10-25 13:12:35 +0200

    document Grid fields

--- nastran_to_kratos/nastran/grid.py
+++ nastran_to_kratos/nastran/grid.py
@@ -10,13 +10,30 @@ class Grid(_NastranEntry):
     """A geometric grid point."""
 
     id: int
+    """Grid point identification number."""
+
     cp: int | None = None
+    """Identification number of coordinate system in which the location is defined."""
+
     x1: float = 0.0
+    """Location of the grid point in coordinate system CP in the x-axis."""
+
     x2: float = 0.0
+    """Location of the grid point in coordinate system CP in the y-axis."""
+
     x3: float = 0.0
+    """Location of the grid point in coordinate system CP in the z-axis."""
+
     cd: int | None = None
+    """Identification number of coordinate system in which the displacements, degrees-of-freedom,
+    constraints, and solution vectors are defined at the grid point."""
+
     ps: str | None = None
+    """Permanent single-point constraints associated with the grid point (example: "234", "123456",
+    "135")."""
+
     seid: int = 0
+    """Superelement identification number."""
 
     @classmethod
     def read(cls, raw_entry: list[str]) -> Grid:
