4932fdc4682d519d84f66be91a46da6b81fb33f6
unexcellent <>
2024-11-27 15:10:12 +0100

    simplify Model.to_mdpa()

--- nastran_to_kratos/kratos/model/model.py
+++ nastran_to_kratos/kratos/model/model.py
@@ -21,30 +21,19 @@ class Model:
         """Export this model to a list of string compatible with the kratos .mdpa files."""
         mdpa_content = []
 
-        if len(self.properties) != 0:
-            mdpa_content.extend(_properties_to_mdpa(self.properties))
-            mdpa_content.append("")
-
-        if len(self.nodes) != 0:
-            mdpa_content.extend(_nodes_to_mdpa(self.nodes))
-            mdpa_content.append("")
-
-        if len(self.elements) != 0:
-            mdpa_content.extend(_elements_to_mdpa(self.elements))
-            mdpa_content.append("")
-
-        if len(self.conditions) != 0:
-            mdpa_content.extend(_conditions_to_mdpa(self.conditions))
-            mdpa_content.append("")
-
-        if len(self.sub_models) != 0:
-            mdpa_content.extend(_submodels_to_mdpa(self.sub_models))
-            mdpa_content.append("")
+        mdpa_content.extend(_properties_to_mdpa(self.properties))
+        mdpa_content.extend(_nodes_to_mdpa(self.nodes))
+        mdpa_content.extend(_elements_to_mdpa(self.elements))
+        mdpa_content.extend(_conditions_to_mdpa(self.conditions))
+        mdpa_content.extend(_submodels_to_mdpa(self.sub_models))
 
         return _remove_empty_last_row(mdpa_content)
 
 
 def _properties_to_mdpa(properties: dict[int, dict[str, float]]) -> list[str]:
+    if properties == {}:
+        return []
+
     mdpa_content = []
 
     for property_id, property_ in properties.items():
@@ -56,10 +45,13 @@ def _properties_to_mdpa(properties: dict[int, dict[str, float]]) -> list[str]:
         mdpa_content.append("End Properties")
         mdpa_content.append("")
 
-    return _remove_empty_last_row(mdpa_content)
+    return mdpa_content
 
 
 def _nodes_to_mdpa(nodes: dict[int, Node]) -> list[str]:
+    if nodes == {}:
+        return []
+
     mdpa_content = []
     mdpa_content.append("Begin Nodes")
 
@@ -67,10 +59,14 @@ def _nodes_to_mdpa(nodes: dict[int, Node]) -> list[str]:
         mdpa_content.append(_indent(node.to_mdpa(node_id), 1))
 
     mdpa_content.append("End Nodes")
+    mdpa_content.append("")
     return mdpa_content
 
 
 def _elements_to_mdpa(elements: dict[str, dict[int, Element]]) -> list[str]:
+    if elements == {}:
+        return []
+
     mdpa_content = []
     for element_id, element in elements.items():
         mdpa_content.append(f"Begin Elements {element_id}")
@@ -81,10 +77,13 @@ def _elements_to_mdpa(elements: dict[str, dict[int, Element]]) -> list[str]:
         mdpa_content.append("End Elements")
         mdpa_content.append("")
 
-    return _remove_empty_last_row(mdpa_content)
+    return mdpa_content
 
 
 def _conditions_to_mdpa(conditions: dict[str, dict[int, Condition]]) -> list[str]:
+    if conditions == {}:
+        return []
+
     mdpa_content = []
 
     for condition_id, condition in conditions.items():
@@ -96,10 +95,13 @@ def _conditions_to_mdpa(conditions: dict[str, dict[int, Condition]]) -> list[str
         mdpa_content.append("End Conditions")
         mdpa_content.append("")
 
-    return _remove_empty_last_row(mdpa_content)
+    return mdpa_content
 
 
 def _submodels_to_mdpa(submodels: dict[str, SubModel]) -> list[str]:
+    if submodels == {}:
+        return []
+
     mdpa_content = []
 
     for submodel_id, submodel in submodels.items():
@@ -110,4 +112,4 @@ def _submodels_to_mdpa(submodels: dict[str, SubModel]) -> list[str]:
         mdpa_content.append("End SubModelPart")
         mdpa_content.append("")
 
-    return _remove_empty_last_row(mdpa_content)
+    return mdpa_content
