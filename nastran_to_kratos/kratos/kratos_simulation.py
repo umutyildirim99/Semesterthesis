9fc434505b6692191004e17a3bf5cb10701bf899
unexcellent <>
2024-11-29 14:22:28 +0100

    implement KratosSimulation.write_to_directory() for materials

--- nastran_to_kratos/kratos/kratos_simulation.py
+++ nastran_to_kratos/kratos/kratos_simulation.py
@@ -1,6 +1,8 @@
+import json
 from dataclasses import dataclass
 from pathlib import Path
 
+from .material import Material
 from .model import Model
 
 
@@ -8,15 +10,36 @@ from .model import Model
 class KratosSimulation:
     """All data for writing kratos simulation files."""
 
-    model: Model
+    model: Model | None = None
+    "The model used in the simulation."
+
+    materials: list[Material] | None = None
+    "The materials for each model part."
 
     def write_to_directory(self, output_dir: Path) -> None:
         """Store the simulation as files in an output directory."""
         if not output_dir.is_dir():
             output_dir.mkdir()
 
-        with (output_dir / "model.mdpa").open("w") as model_file:
-            model_file.writelines(_add_line_break_to_every_line(self.model.to_mdpa()))
+        _write_model_file(self.model, output_dir / "model.mdpa")
+        _write_materials_file(self.materials, output_dir / "materials.json")
+
+
+def _write_model_file(model: Model | None, path: Path) -> None:
+    if model is None:
+        return
+    with path.open("w") as model_file:
+        model_file.writelines(_add_line_break_to_every_line(model.to_mdpa()))
+
+
+def _write_materials_file(materials: list[Material] | None, path: Path) -> None:
+    if materials is None:
+        return
+
+    materials_dict = {"properties": [material.to_json() for material in materials]}
+
+    with path.open("w") as materials_file:
+        json.dump(materials_dict, materials_file)
 
 
 def _add_line_break_to_every_line(lines: list[str]) -> list[str]:
--- test/kratos/test_kratos_simulation.py
+++ test/kratos/test_kratos_simulation.py
@@ -1,9 +1,11 @@
 from pathlib import Path
+import json
 
 import pytest
 
 from nastran_to_kratos.kratos import KratosSimulation
 from nastran_to_kratos.kratos.model import Model, Node, Element, Condition, SubModel
+from nastran_to_kratos.kratos.material import Material
 
 
 def test_write_to_directory__x_movable_rod__model(tmp_path):
@@ -31,6 +33,32 @@ def test_write_to_directory__x_movable_rod__model(tmp_path):
 
     with ground_truth_path.open() as f:
         ground_truth = f.readlines()
+
+    assert actual == ground_truth
+
+
+def test_write_to_directory__x_movable_rod__materials(tmp_path):
+    output_dir = tmp_path / "x_movable_rod"
+    ground_truth_path = Path(__file__).parent.parent.parent / "examples" / "Materials.json"
+
+    kratos_simulation = KratosSimulation(
+        materials=[
+            Material(
+                model_part_name="Structure.Truss",
+                properties_id=0,
+                material_name="MAT1_1",
+                constitutive_law="TrussConstitutiveLaw",
+                variables={"YOUNG_MODULUS": 210000.0, "DENSITY": 7850, "CROSS_AREA": 350},
+            )
+        ]
+    )
+
+    kratos_simulation.write_to_directory(output_dir)
+    with (output_dir / "materials.json").open() as f:
+        actual = json.load(f)
+
+    with ground_truth_path.open() as f:
+        ground_truth = json.load(f)
 
     assert actual == ground_truth
 
