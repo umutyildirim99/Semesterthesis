8b5238a9ad6759043f95eb4c8dbc6289b3368810
unexcellent <>
2025-01-26 20:29:13 +0100

    document kratos_to_nastran()

--- nastran_to_kratos/kratos_to_nastran.py
+++ nastran_to_kratos/kratos_to_nastran.py
@@ -4,8 +4,24 @@ from .kratos import KratosSimulation
 from .translation_layer import TranslationLayer
 
 
-def kratos_to_nastran(kratos_directory: Path, output_file: Path) -> None:
-    """Convert a simulation in the kratos format into nastran."""
+def kratos_to_nastran(kratos_directory: Path | str, output_file: Path | str) -> None:
+    """Convert a simulation in the kratos format into nastran.
+
+    Args:
+        kratos_directory : Directory containing the kratos simulation files. The directory must
+            contain the files `material.json`, `model.mdpa` and `simulation_parameters.json`
+        output_file : The path to the target file of the nastran simulation.
+
+    Example:
+    ```python
+    from nastran_to_kratos import kratos_to_nastran
+
+    kratos_to_nastran("/path/to/kratos", "/path/to/output/file.bdf")
+    ```
+    """
+    kratos_directory = Path(kratos_directory)
+    output_file = Path(output_file)
+
     kratos = KratosSimulation.from_directory(kratos_directory)
     translation_layer = TranslationLayer.from_kratos(kratos)
     nastran = translation_layer.to_nastran()
